b11fa02a3a133ddd61373cd0772fe501992328dc
zhang.weifeng <zhangweifeng@diinom.com>
2025-04-27 14:03:36 +0800

    chore: 调整项目结构

--- examples/webgl_gpgpu_water/vite.config.ts
+++ examples/webgl_gpgpu_water/vite.config.ts
@@ -2,6 +2,5 @@ import { defineConfig } from "vite";
 
 console.log("Vite config for webgl_gpgpu_water example");
 export default defineConfig({
-  // root: "./",
-  publicDir: "../../common",
+  publicDir: "../../public",
 });
