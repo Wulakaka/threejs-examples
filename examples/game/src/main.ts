b8d40467adc7a1d7d6f824b11b30b51ff66d02b7
zhang.weifeng <zhangweifeng@diinom.com>
2025-04-28 15:12:35 +0800

    feat(game): initialize game example with Three.js and GLTF models

---
+++ examples/game/src/main.ts
@@ -0,0 +1,31 @@
+import * as THREE from "three";
+import { GLTFLoader, type GLTF } from "three/examples/jsm/Addons.js";
+const manager = new THREE.LoadingManager();
+
+manager.onLoad = init;
+const models: {
+  [key: string]: {
+    url: string;
+    gltf?: GLTF;
+  };
+} = {
+  pig: { url: "/models/animals/Pig.glb" },
+  cow: { url: "/models/animals/Cow.glb" },
+  llama: { url: "/models/animals/Llama.glb" },
+  pug: { url: "/models/animals/Pug.glb" },
+  sheep: { url: "/models/animals/Sheep.glb" },
+  zebra: { url: "/models/animals/Zebra.glb" },
+  horse: { url: "/models/animals/Horse.glb" },
+  knight: { url: "/models/knight/KnightCharacter.glb" },
+};
+{
+  const gltfLoader = new GLTFLoader(manager);
+  for (const model of Object.values(models)) {
+    gltfLoader.load(model.url, (gltf) => {
+      model.gltf = gltf;
+    });
+  }
+}
+function init() {
+  // TBD
+}
