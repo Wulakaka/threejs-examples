9d9a8a5dde1aa93b5f18b3b01ed57aa2aea526b1
zhang.weifeng <zhangweifeng@diinom.com>
2025-12-10 15:08:13 +0800

    feat: 添加 tsl demo

---
+++ gpu/1-demo/src/getMaterial.ts
@@ -0,0 +1,13 @@
+import {color, positionLocal, sin, time} from "three/tsl";
+import {MeshBasicNodeMaterial} from "three/webgpu";
+
+export function getMaterial() {
+  // 最小 TSL 节点材质；随时间变色
+  const R = sin(positionLocal.x.add(time)).add(1).mul(0.5);
+  const G = sin(positionLocal.y.add(time)).add(1).mul(0.5);
+  const B = sin(positionLocal.x.add(time)).add(1).mul(0.5);
+  const material = new MeshBasicNodeMaterial({
+    colorNode: color(R, G, B),
+  });
+  return material;
+}
---
+++ gpu/1-demo/src/main.ts
@@ -0,0 +1,52 @@
+import {Mesh, PerspectiveCamera, PlaneGeometry, Scene} from "three";
+import {WebGPURenderer} from "three/webgpu";
+import {getMaterial} from "./getMaterial";
+
+async function boot() {
+  const container = document.createElement("div");
+  document.body.appendChild(container);
+
+  let renderer: WebGPURenderer;
+
+  renderer = new WebGPURenderer({
+    antialias: true,
+    alpha: true,
+  });
+
+  await renderer.init(); // WebGPU 需显式初始化
+
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  container.appendChild(renderer.domElement);
+
+  // 场景和相机
+  const scene = new Scene();
+  const camera = new PerspectiveCamera(
+    50,
+    container.clientWidth / container.clientHeight,
+    0.1,
+    100
+  );
+  camera.position.z = 2.5;
+
+  // 几何 + 最小 TSL 节点材质；随时间变色
+  const geometry = new PlaneGeometry(2, 2);
+  const material = getMaterial();
+
+  scene.add(new Mesh(geometry, material));
+
+  // 自适应
+  function resize() {
+    const w = window.innerWidth;
+    const h = window.innerHeight;
+    camera.aspect = w / h;
+    camera.updateProjectionMatrix();
+    renderer.setSize(w, h);
+  }
+  window.addEventListener("resize", resize);
+
+  // 动画循环
+  renderer.setAnimationLoop(() => renderer.render(scene, camera));
+}
+
+boot();
