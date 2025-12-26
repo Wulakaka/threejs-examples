31e5a0b6501f44003bad184da056e11467c1eac1
zhang.weifeng <zhangweifeng@diinom.com>
2025-12-26 17:51:05 +0800

    feat(firework): 增加图形逻辑

--- gpu/7-firework/src/main.ts
+++ gpu/7-firework/src/main.ts
@@ -1,6 +1,6 @@
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js";
 import GUI from "three/examples/jsm/libs/lil-gui.module.min.js";
-import {float, instancedBufferAttribute} from "three/tsl";
+import {float, instancedBufferAttribute, texture, uv, vec4} from "three/tsl";
 import * as THREE from "three/webgpu";
 
 const gui = new GUI({width: 340});
@@ -71,31 +71,68 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 /**
  * Firework
  */
+const textures = [
+  textureLoader.load("./particles/1.png"),
+  textureLoader.load("./particles/2.png"),
+  textureLoader.load("./particles/3.png"),
+  textureLoader.load("./particles/4.png"),
+  textureLoader.load("./particles/5.png"),
+  textureLoader.load("./particles/6.png"),
+  textureLoader.load("./particles/7.png"),
+  textureLoader.load("./particles/8.png"),
+];
+
 const createFirework = (
   count: number,
   position: THREE.Vector3,
-  size: number
+  size: number,
+  map: THREE.Texture,
+  radius: number,
+  color: THREE.Color
 ) => {
   const positionArray = new Float32Array(count * 3);
+  const randomArray = new Float32Array(count);
 
   for (let i = 0; i < count; i++) {
     const i3 = i * 3;
-    positionArray[i3 + 0] = Math.random() - 0.5;
-    positionArray[i3 + 1] = Math.random() - 0.5;
-    positionArray[i3 + 2] = Math.random() - 0.5;
+
+    const spherical = new THREE.Spherical(
+      radius * (0.75 + Math.random() * 0.25),
+      Math.random() * Math.PI,
+      Math.random() * Math.PI * 2
+    );
+
+    const position = new THREE.Vector3();
+    position.setFromSpherical(spherical);
+
+    positionArray[i3 + 0] = position.x;
+    positionArray[i3 + 1] = position.y;
+    positionArray[i3 + 2] = position.z;
+
+    randomArray[i] = Math.random();
   }
 
   const positionAttribute = new THREE.InstancedBufferAttribute(
     positionArray,
     3
   );
+  const randomAttribute = new THREE.InstancedBufferAttribute(randomArray, 1);
+
+  const textureAlpha = texture(map, uv()).r;
 
   // Material
   const material = new THREE.SpriteNodeMaterial({
     positionNode: instancedBufferAttribute(positionAttribute),
     sizeAttenuation: true,
+    // map: map,
+    transparent: true,
+    colorNode: vec4(color, textureAlpha),
+    depthWrite: false,
+    blending: THREE.AdditiveBlending,
   });
-  material.scaleNode = float(size);
+  material.scaleNode = float(size).mul(
+    instancedBufferAttribute(randomAttribute)
+  );
 
   // Points
   const firework = new THREE.Sprite(material);
@@ -105,7 +142,14 @@ const createFirework = (
   scene.add(firework);
 };
 
-createFirework(100, new THREE.Vector3(0, 0, 0), 0.1);
+createFirework(
+  100,
+  new THREE.Vector3(0, 0, 0),
+  0.1,
+  textures[7],
+  1,
+  new THREE.Color("#8affff")
+);
 renderer.init().then(() => {
   /**
    * Animate
