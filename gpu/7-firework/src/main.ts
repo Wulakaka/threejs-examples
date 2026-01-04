ba4af3715c1b966d509d5c7fc0c64ffc9b0e1826
zhang.weifeng <zhangweifeng@diinom.com>
2026-01-04 11:53:39 +0800

    feat(firework): 完善效果

--- gpu/7-firework/src/main.ts
+++ gpu/7-firework/src/main.ts
@@ -1,17 +1,20 @@
 import gsap from "gsap";
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js";
 import {Inspector} from "three/examples/jsm/inspector/Inspector.js";
+import {bloom} from "three/examples/jsm/tsl/display/BloomNode.js";
 import {
-  atan,
   color,
-  cos,
   float,
   Fn,
+  If,
   instancedBufferAttribute,
+  luminance,
   min,
   mix,
+  pass,
   pow,
   sin,
+  step,
   texture,
   uniform,
   uv,
@@ -99,52 +102,20 @@ const textures = [
   textureLoader.load("./particles/8.png"),
 ];
 
-const from = new THREE.Vector3(-1, -1, -1);
-const axesHelper = new THREE.AxesHelper();
-axesHelper.position.copy(from);
-scene.add(axesHelper);
-
-const createShootingStar = (
-  progress: ReturnType<typeof uniform>,
-  position: THREE.Vector3,
-  start: THREE.Vector3
-) => {
-  const uProgress = progress.remap(0, 0.4).clamp(0, 1);
-  const from = vec3(start);
-  const target = vec3(position);
-  const strength = from.y.sub(target.y);
-  const toTarget = target.sub(from);
-  const speed = toTarget.length();
-  const angle = atan(toTarget.x, toTarget.z);
-  const x = sin(angle).mul(uProgress).mul(speed).add(from.x);
-  const y = strength.mul(uProgress.sub(1).pow(2)).add(target.y);
-  const z = cos(angle).mul(uProgress).mul(speed).add(from.z);
-  const positionNode = vec3(x, y, z);
-
-  const material = new THREE.SpriteNodeMaterial({
-    colorNode: uv().sub(vec2(0.5)).length().step(0.5).pow(2).oneMinus(),
-    scaleNode: float(0.1),
-    sizeAttenuation: true,
-    positionNode: positionNode,
-  });
-  const sprite = new THREE.Sprite(material);
-  scene.add(sprite);
-
-  const destroy = () => {
-    scene.remove(sprite);
-    material.dispose();
-  };
-
-  return destroy;
-};
+const from = new THREE.Vector3(0, -2, 0);
+// const axesHelper = new THREE.AxesHelper();
+// axesHelper.position.copy(from);
+// scene.add(axesHelper);
 
 const geometry = new THREE.IcosahedronGeometry(1, 15);
 
 const positionArray = geometry.getAttribute("position").array;
 const count = geometry.getAttribute("position").count;
-const randomArray = new Float32Array(count);
 
+const randomArray = new Float32Array(count);
 const timeMultipliersArray = new Float32Array(count);
+const indexArray = new Float32Array(count);
+
 const offset = 0.15;
 
 for (let i = 0; i < count; i++) {
@@ -156,6 +127,8 @@ for (let i = 0; i < count; i++) {
   randomArray[i] = Math.random();
 
   timeMultipliersArray[i] = 1 + Math.random();
+
+  indexArray[i] = i;
 }
 
 const positionAttribute = new THREE.InstancedBufferAttribute(positionArray, 3);
@@ -166,6 +139,8 @@ const timeMultipliersAttribute = new THREE.InstancedBufferAttribute(
   1
 );
 
+const indexAttribute = new THREE.InstancedBufferAttribute(indexArray, 1);
+
 const pos = instancedBufferAttribute(positionAttribute);
 
 // 0-1
@@ -173,23 +148,49 @@ const aRandom = instancedBufferAttribute(randomAttribute);
 
 const aTimeMultiplier = instancedBufferAttribute(timeMultipliersAttribute);
 
-const createExploding = (
-  progress: ReturnType<typeof uniform>,
-  detail: number,
+const aIndex = instancedBufferAttribute(indexAttribute);
+
+const createFirework = (
   position: THREE.Vector3,
   size: number,
   map: THREE.Texture,
   radius: number,
   c: THREE.Color
 ) => {
-  // const uProgress = uniform(0);
-  const uProgress = progress.remap(0.4, 1).clamp(0, 1);
+  const uProgress = uniform(0);
 
   const textureAlpha = texture(map, uv()).r;
 
-  const positionNode = Fn(
-    ([pos, progress]: [ReturnType<typeof vec3>, ReturnType<typeof float>]) => {
-      const p = pos.mul(vec3(radius)).toVar();
+  const shootingPos = Fn(
+    ({
+      progress,
+      start,
+      target,
+    }: {
+      progress: ReturnType<typeof float>;
+      start: ReturnType<typeof vec3>;
+      target: ReturnType<typeof vec3>;
+    }) => {
+      const x = mix(start.x, target.x, progress);
+      const z = mix(start.z, target.z, progress);
+      // Parabola
+      const strength = start.y.sub(target.y);
+      const y = strength.mul(progress.sub(1).pow(2)).add(target.y);
+      return vec3(x, y, z);
+    }
+  );
+
+  const explodingPos = Fn(
+    ({
+      localPos,
+      positionOffset,
+      progress,
+    }: {
+      localPos: ReturnType<typeof vec3>;
+      positionOffset: ReturnType<typeof vec3>;
+      progress: ReturnType<typeof float>;
+    }) => {
+      const p = localPos.mul(vec3(radius)).toVar();
 
       // Exploding
       const explodingProgress = progress.remap(0, 0.1).toVar();
@@ -203,11 +204,52 @@ const createExploding = (
       fallingProgress.assign(pow(fallingProgress.oneMinus(), 3).oneMinus());
       p.y.subAssign(fallingProgress.mul(0.2));
 
+      return p.add(positionOffset);
+    }
+  );
+
+  const getPos = Fn(
+    ({
+      localPos,
+      progress,
+      target,
+      from,
+    }: {
+      localPos: ReturnType<typeof vec3>;
+      progress: ReturnType<typeof float>;
+      target: ReturnType<typeof vec3>;
+      from: ReturnType<typeof vec3>;
+    }) => {
+      const p = pos.toVar();
+      If(progress.lessThan(0.4), () => {
+        const shootingProgress = progress.remap(0, 0.4).clamp(0, 1);
+
+        p.assign(
+          shootingPos({
+            progress: shootingProgress,
+            start: from,
+            target: target,
+          })
+        );
+      }).Else(() => {
+        const explodingProgress = progress
+          .remap(0.4, 1)
+          .clamp(0, 1)
+          .mul(aTimeMultiplier);
+
+        p.assign(
+          explodingPos({
+            localPos,
+            positionOffset: target,
+            progress: explodingProgress,
+          })
+        );
+      });
       return p;
     }
   );
 
-  const scaleNode = Fn(
+  const explodingScale = Fn(
     ([size, progress]: [
       ReturnType<typeof float>,
       ReturnType<typeof float>,
@@ -234,24 +276,92 @@ const createExploding = (
     }
   );
 
-  const explodingProgress = uProgress.mul(aTimeMultiplier);
+  const getScale = Fn(
+    ({
+      progress,
+      size,
+    }: {
+      progress: ReturnType<typeof float>;
+      size: ReturnType<typeof float>;
+    }) => {
+      const p = progress.toVar();
+      const scale = float();
+      If(p.lessThan(0.4), () => {
+        const progress = p.remap(0, 0.4).clamp(0, 1);
+        const fromProgress = progress.remap(0, 0.1).clamp(0, 1).pow(2);
+        const toProgress = progress
+          .remap(0.1, 0.8)
+          .clamp(0, 1)
+          .pow(2)
+          .oneMinus();
+
+        scale.assign(float(0.05).mul(mix(fromProgress, toProgress, progress)));
+      }).Else(() => {
+        const explodingProgress = p
+          .remap(0.4, 1)
+          .clamp(0, 1)
+          .mul(aTimeMultiplier);
+        scale.assign(explodingScale(float(size), explodingProgress));
+      });
+      return scale;
+    }
+  );
+
+  const getColor = Fn(
+    ({
+      progress,
+      baseColor,
+      alpha,
+    }: {
+      progress: ReturnType<typeof float>;
+      baseColor: ReturnType<typeof vec4>;
+      alpha: ReturnType<typeof float>;
+    }) => {
+      const shootingStrength = uv()
+        .sub(vec2(0.5))
+        .length()
+        .step(0.5)
+        .oneMinus();
+      const shootingAlpha = step(0.5, aIndex).oneMinus();
+      const shootingColor = vec4(
+        baseColor,
+        shootingAlpha.mul(shootingStrength)
+      );
+      const shootingLuminance = luminance(shootingColor.rgb);
+      shootingColor.assign(shootingColor.div(shootingLuminance).mul(1.2));
+
+      const explodingColor = vec4(baseColor, alpha).toVar();
+      const explodingLuminance = luminance(explodingColor.rgb);
+      explodingColor.assign(explodingColor.div(explodingLuminance).mul(1.1));
+
+      return mix(shootingColor, explodingColor, progress.step(0.4));
+    }
+  );
 
   // Material
   const material = new THREE.SpriteNodeMaterial({
-    positionNode: positionNode(pos, explodingProgress),
+    positionNode: getPos({
+      localPos: pos,
+      progress: uProgress,
+      target: vec3(position),
+      from: vec3(from),
+    }),
     sizeAttenuation: true,
     transparent: true,
-    colorNode: vec4(color(c), textureAlpha),
+    colorNode: getColor({
+      progress: uProgress,
+      baseColor: color(c),
+      alpha: textureAlpha,
+    }),
     depthWrite: false,
-    blending: THREE.AdditiveBlending,
-    scaleNode: scaleNode(size, explodingProgress),
+    // blending: THREE.AdditiveBlending,
+    scaleNode: getScale({progress: uProgress, size}),
   });
 
   // Points
   const firework = new THREE.Sprite(material);
   // 需要指定 count 才有效果
   firework.count = count;
-  firework.position.copy(position);
   scene.add(firework);
 
   const destroy = () => {
@@ -259,12 +369,17 @@ const createExploding = (
     material.dispose();
   };
 
-  return destroy;
+  gsap.to(uProgress, {
+    value: 1,
+    duration: 5,
+    ease: "linear",
+    onComplete: () => {
+      destroy();
+    },
+  });
 };
 
 const createRandomFirework = () => {
-  const detail = Math.round(10 + Math.random() * 10);
-  // ...
   const position = new THREE.Vector3(
     (Math.random() - 0.5) * 2,
     Math.random(),
@@ -279,34 +394,26 @@ const createRandomFirework = () => {
   const color = new THREE.Color();
   color.setHSL(Math.random(), 1, 0.7);
 
-  const progress = uniform(0);
-  const destroyShooting = createShootingStar(progress, position, from);
-  const destroyExploding = createExploding(
-    progress,
-    detail,
-    position,
-    size,
-    texture,
-    radius,
-    color
-  );
-  gsap.to(progress, {
-    value: 1,
-    duration: 5,
-    ease: "linear",
-    onComplete: () => {
-      destroyShooting();
-      destroyExploding();
-    },
-  });
+  createFirework(position, size, texture, radius, color);
 };
 
 window.addEventListener("click", createRandomFirework);
+
+const postProcessing = new THREE.PostProcessing(renderer);
+
+const scenePass = pass(scene, camera);
+
+const scenePassColor = scenePass.getTextureNode();
+
+const bloomPass = bloom(scenePassColor, 1, 0.05, 1);
+
+postProcessing.outputNode = scenePassColor.add(bloomPass);
 
 renderer.setAnimationLoop((t) => {
   // Update controls
   controls.update();
 
   // Render
-  renderer.render(scene, camera);
+  // renderer.render(scene, camera);
+  postProcessing.render();
 });
