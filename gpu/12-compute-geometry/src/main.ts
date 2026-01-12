03c65eeb2758401ab47ed545eb2b7f19744e1c7c
zhang.weifeng <zhangweifeng@diinom.com>
2026-01-12 14:37:56 +0800

    doc(compute geometry): 增加注释

--- gpu/12-compute-geometry/src/main.ts
+++ gpu/12-compute-geometry/src/main.ts
@@ -34,23 +34,30 @@ const brushStrength = uniform(0.22);
 
 init();
 
+// geometryNode 默认会传递 renderer、 geometry、 object，可能还有其他的
+// TODO 这里没有使用 renderer.compute(updateCompute) 的方式是为什么？
 const jelly = Fn(({renderer, geometry, object}) => {
   const count = geometry.attributes.position.count;
 
   // Create storage buffer attribute for modified position.
 
+  // 原始位置信息
   const positionBaseAttribute = geometry.attributes
-    .position as THREE.StorageBufferAttribute;
+    .position as THREE.BufferAttribute;
+
+  // 最终显示的位置
   const positionStorageBufferAttribute = new THREE.StorageBufferAttribute(
     count,
     3
   );
   const speedBufferAttribute = new THREE.StorageBufferAttribute(count, 3);
 
+  // 存储到 geometry 的 attribute 中，这样在设置 positionNode 时可以直接用 attribute("storagePosition")
   geometry.setAttribute("storagePosition", positionStorageBufferAttribute);
 
   // Attributes
-
+  // 转换为 storage buffer， 以便在 compute shader 中读写
+  // 不转换的话只能读取，不能写入
   const positionAttribute = storage(positionBaseAttribute, "vec3", count);
   const positionStorageAttribute = storage(
     positionStorageBufferAttribute,
@@ -73,7 +80,7 @@ const jelly = Fn(({renderer, geometry, object}) => {
 
   const computeInit = Fn(() => {
     // Modified storage position starts out the same as the base position.
-
+    // 翻译：初始化原始位置
     currentPosition.assign(basePosition);
   })().compute(count);
 
@@ -81,13 +88,19 @@ const jelly = Fn(({renderer, geometry, object}) => {
 
   const computeUpdate = Fn(() => {
     // pinch
+    // 捏取效果
 
+    // 通过 w 通道判断是否启用捏取效果
     If(pointerPosition.w.equal(1), () => {
+      // 创建世界矩阵，再乘以当前顶点位置，得到顶点的世界位置
       const worldPosition = objectWorldMatrix(object).mul(currentPosition);
 
+      // 指针到顶点的距离
       const dist = worldPosition.distance(pointerPosition.xyz);
+      // 指向 pointer 的方向
       const direction = pointerPosition.xyz.sub(worldPosition).normalize();
 
+      // brushSize 内的才有 power
       const power = brushSize.sub(dist).max(0).mul(brushStrength);
 
       currentPosition.addAssign(direction.mul(power));
@@ -95,14 +108,20 @@ const jelly = Fn(({renderer, geometry, object}) => {
 
     // compute ( jelly )
 
+    // 距离初始位置
     const distance = basePosition.distance(currentPosition);
+    // 力等于弹力系数乘以距离，再乘以方向向量
+    // TODO 这里的方向如果 normalize 将不会渲染图形，为什么？
     const force = elasticity
       .mul(distance)
       .mul(basePosition.sub(currentPosition));
 
+    // += force
     currentSpeed.addAssign(force);
+    // *= damping
     currentSpeed.mulAssign(damping);
 
+    // position += speed
     currentPosition.addAssign(currentSpeed);
   })()
     .compute(count)
@@ -134,9 +153,12 @@ function init() {
   // background
 
   const bgColor = screenUV.y.mix(color(0x9f87f7), color(0xf2cdcd));
+  // 只取距离中心 0.3 到 0.8 之间的值，并且映射为 0 到 1，再反转
+  // 中心为 1，边缘为 0
   const bgVignette = screenUV.distance(0.5).remapClamp(0.3, 0.8).oneMinus();
   const bgIntensity = 4;
 
+  // 用两个颜色相乘
   scene.backgroundNode = bgColor.mul(
     bgVignette.mul(color(0xa78ff6).mul(bgIntensity))
   );
@@ -150,6 +172,7 @@ function init() {
 
       const material = new THREE.MeshNormalNodeMaterial();
       material.geometryNode = jelly();
+      // 因为前面将 storagePosition 存储到了 geometry 的属性中
       material.positionNode = attribute("storagePosition");
 
       // apply the material to the mesh
@@ -197,7 +220,7 @@ function onPointerMove(event: PointerEvent) {
   if (intersects.length > 0) {
     const intersect = intersects[0];
 
-    pointerPosition.value.copy(intersect.point);
+    pointerPosition.value.copy(new THREE.Vector4(...intersect.point, 0));
     pointerPosition.value.w = 1; // enable
   } else {
     pointerPosition.value.w = 0; // disable
